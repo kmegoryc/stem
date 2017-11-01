264da1735306c54d27fb17f4d781338087e1173d
Keren Megory-Cohen <keren.megorycohen@colorado.edu>
2017-11-01 02:06:03 -0600

    Delete ignored files and style modules

--- src/clj/stem/styles/core.clj
+++ src/clj/stem/styles/core.clj
@@ -1,19 +1,25 @@
 (ns stem.styles.core
   (:require [garden.def :refer [defstyles]]
+            [garden.color :as color :refer [hsl rgb rgba]]
             [garden.units :refer [px em percent]]
             [greenhouse.grid :refer [column span clearfix center stack align on cycle-props]]))
 
+(def module-styles
+  {:position :relative
+   :padding (px 40)
+   :border-radius (px 5)
+   :border "1px solid lightgrey"
+   :margin [[(px 20) 0]]
+   :box-shadow [[0 0 (px 15) (rgba 0 0 0 0.2)]]})
+
 (def results-module
-  [:div.results-module {:position :relative
-                        :padding (px 40)
-                        :border-radius (px 5)
-                        :border "1px solid lightgrey"
-                        :margin [[(px 20) 0]]}
+  [:div.results-module
+   module-styles
    [:div.teacher-header {:margin-top (px 10)}]])
 
 (def response-module
-  [:div.response-module {:position :relative
-                         :padding [[(px 40) 0]]}])
+  [:div.response-module
+   module-styles])
 
 (def variables
   {:max-width (px 1200)
